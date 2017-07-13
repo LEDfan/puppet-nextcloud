ea3e32dfafaabd77bcf835748b0bd739fce28087
Tobia De Koninck <tobia@inuits.eu>
2017-07-13 12:35:10 +0200

    Remove obsolete modules

--- vagrant/modules/firewall/spec/unit/documentation/readme_spec.rb
+++
@@ -1,6 +0,0 @@
-describe 'formatting in README.markdown' do
-  it 'should not contain badly formatted heading markers' do
-    content = File.read('README.markdown')
-    expect(content).to_not match /^#+[^# ]/
-  end
-end
--- vagrant/modules/icinga/lib/parser/functions/iciuserstest.rb
+++
@@ -1,5 +0,0 @@
-module Puppet::Parser::Functions
-  newfunction(:iciuserstest, :type => :rvalue) do
-    %x{cut -d ':' -f 1 /etc/icinga/htpasswd.users | tr '\n' ',' | sed -e 's/,$/\n/g'}.chomp
-  end
-end
