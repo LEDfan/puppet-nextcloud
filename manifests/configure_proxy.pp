56f8c981dc7c45c4ed83c7a24adb62a99b0cd3e6
Tobia De Koninck <tobia@inuits.eu>
2017-07-19 13:29:39 +0200

    Add option to configure proxy (https://docs.nextcloud.com/server/12/admin_manual/configuration_server/reverse_proxy_configuration.html)

---
+++ manifests/configure_proxy.pp
@@ -0,0 +1,12 @@
+# class to configure proxies inside nextcloud
+# see https://docs.nextcloud.com/server/12/admin_manual/configuration_server/reverse_proxy_configuration.html
+class nextcloud::configure_proxy (
+    $proxy_trusted_proxies,
+    $proxy_overwritehots,
+    $proxy_overwriteprotocol){
+
+  nextcloud::import_config { 'import_proxy_config':
+    file_name => template('nextcloud/nextcloud-proxy.json.erb')
+  }
+
+}
