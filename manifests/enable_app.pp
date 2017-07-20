badf0df8abe1a1da12fa2ea9ed68302452c98ad0
Tobia De Koninck <tobia@inuits.eu>
2017-07-20 10:53:18 +0200

    Only enable app when it's not already enabled, and only eimport settings when it's not already imported

--- manifests/enable_app.pp
+++ manifests/enable_app.pp
@@ -3,6 +3,10 @@ define nextcloud::enable_app ($app_name) {
   exec { "enale-nc-app-${app_name}":
     command => "/usr/bin/php /var/www/html/nextcloud/occ app:enable ${app_name}",
     user    => apache,
-    group   => apache
+    group   => apache,
+    # this regex will check if the app is disabled, only then the app will be enabled
+    # the regix will check if the appname appears after the "Disabled" string
+    # note that this will fail if Nextcloud changes the output of the occ app:list command
+    onlyif  => "/usr/bin/php /var/www/html/nextcloud/occ app:list 2> /dev/null | /usr/bin/grep -Pzo \"Disabled(.|\\s)*${app_name}\""
   }
 }
--- manifests/import_config.pp
+++ manifests/import_config.pp
@@ -7,6 +7,8 @@ define nextcloud::import_config ($file_name){
   exec { "import-nc-config-${title}":
     command => "/usr/bin/php /var/www/html/nextcloud/occ config:import /tmp/nextcloud-import-config-${title}",
     user    => apache,
-    group   => apache
+    group   => apache,
+    # this command will return 0 if the nextcloud config file don't contain all lines in the provided config file
+    onlyif  => "/usr/bin/php /var/www/html/nextcloud/occ config:list > /tmp/config.txt && /usr/bin/grep -Fvf /tmp/config.txt /tmp/nextcloud-import-config-${title}"
   }
 }
