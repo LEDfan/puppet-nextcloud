338467b8826382607fc8b0d4a139580e97ebf72f
Tobia De Koninck <tobia@inuits.eu>
2017-07-20 13:52:13 +0200

    Fix bug with multiple files declared

--- manifests/import_config.pp
+++ manifests/import_config.pp
@@ -3,17 +3,20 @@ define nextcloud::import_config ($file_name){
   file { "/tmp/nextcloud-import-config-${title}":
     ensure  => present,
     content => $file_name,
-  }->
-  file { "match.py-for-${title}":
-    ensure => present,
-    path   => '/tmp/match.py',
-    source => 'puppet:///modules/nextcloud/match.py',
-  }->
+  }
+  if !defined(File['/tmp/match.py']) {
+    file { '/tmp/match.py':
+      ensure => present,
+      path   => '/tmp/match.py',
+      source => 'puppet:///modules/nextcloud/match.py',
+    }
+  }
   exec { "import-nc-config-${title}":
     command => "/usr/bin/php /var/www/html/nextcloud/occ config:import /tmp/nextcloud-import-config-${title}",
     user    => apache,
     group   => apache,
     # this command will return 0 if the nextcloud config file don't contain all lines in the provided config file
-    unless  => "/usr/bin/php /var/www/html/nextcloud/occ config:list --private > /tmp/config.txt && /usr/bin/python /tmp/match.py /tmp/config.txt /tmp/nextcloud-import-config-${title}"
+    unless  => "/usr/bin/php /var/www/html/nextcloud/occ config:list --private > /tmp/config.txt && /usr/bin/python /tmp/match.py /tmp/config.txt /tmp/nextcloud-import-config-${title}",
+    require => [File['/tmp/match.py'], File["/tmp/nextcloud-import-config-${title}"]]
   }
 }
